36aeac968b651a6e0f894781b913370097720f8b
leblancr <rich.leblanc1@proton.me>
2024-09-28 22:01:34 -0400

    commented enums

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # elixer_tutorial
+
+To get shell run iex:
+~/projects/elixer/elixer_tutorial git:[main]
+iex
+Erlang/OTP 27 [erts-15.0.1] [source] [64-bit] [smp:4:4] [ds:4:4:10] [async-threads:1] [jit:ns]
+
+Interactive Elixir (1.17.3) - press Ctrl+C to exit (type h() ENTER for help)
+iex(1)> 
--- tut.exs
+++ tut.exs
@@ -1,47 +1,57 @@
+# prints all the functions available in enum
+IO.puts Enum.__info__(:functions) |>
+          Enum.each(fn({function, arity}) ->
+            IO.puts "#{function} / #{arity}"
+          end)
+
 # check if all are true
 # each element in the list is sent to the function one by one
 res = Enum.all?(["foo", "bar", "hello"], fn(s) ->
-    String.length(s) == 3  # this is applied to each element in the list
+  String.length(s) == 3  # this is applied to each element in the list
 end)
 
-IO.puts "result: #{res}"
+IO.puts "all?([\"foo\", \"bar\", \"hello\"], fn(s) ->
+  String.length(s) == 3
+result: #{res}\n"
 
-# prints all the functions available in enum
-IO.puts Enum.__info__(:functions) |>
-Enum.each(fn({function, arity}) ->
-  IO.puts "#{function} / #{arity}"
-end)
-
-# 
+#
 res = Enum.chunk_every([1, 2, 3, 4, 5, 6], 3)
-IO.puts "result: #{inspect(res)}"
+IO.puts "chunk_every([1, 2, 3, 4, 5, 6], 3)\nresult: #{inspect(res)}\n"
 
-res = Enum.chunk_by(["one", "two", "three", "four", "five"], fn(x) -> String.length(x) end)
-IO.puts "result: #{inspect(res)}"
+# chunk by string length
+res = Enum.chunk_by(["one", "two", "three", "four", "five"], fn(x) ->
+  String.length(x) end)
+IO.puts "chunk_by([\"one\", \"two\", \"three\", \"four\", \"five\"], fn(x) ->
+  String.length(x)
+result: #{inspect(res)}\n"
 
 # Apply function every three items
-res = Enum.map_every([1, 2, 3, 4, 5, 6, 7, 8], 3, fn(x) -> x + 1000 end)
-IO.puts "result: #{inspect(res)}"
+res = Enum.map_every([1, 2, 3, 4, 5, 6, 7, 8], 3, fn(x) ->
+  x + 1000 end)  # end anonymous function
+IO.puts "map_every([1, 2, 3, 4, 5, 6, 7, 8], 3, fn(x) ->
+  x + 1000 end)
+result: #{inspect(res)}\n"
 
+#
 res = Enum.min([5, 3, 0, -1])
-IO.puts "result: #{res}"
+IO.puts "result: #{res}\n"
 
 # apply a function to each element
 res = Enum.filter([1, 2, 3, 4], fn(x) -> rem(x, 2) == 0 end)
-IO.puts "result: #{inspect(res)}"
+IO.puts "result: #{inspect(res)}\n"
 
 # a list and accumulater with initial value 10
 # add each elkement to the previous result
 res = Enum.reduce([1, 2, 3, 4, 5], 1, fn(x, acc) ->
-        IO.puts "x: #{x} + acc: #{acc} = #{x + acc}"
-        x + acc
-    end)
-IO.puts "result: #{res}"
+  IO.puts "x: #{x} + acc: #{acc} = #{x + acc}"
+  x + acc
+end)
+IO.puts "result: #{res}\n"
 
 Enum.reduce(["a","b","c", "d"], "1", fn(x, acc) ->
-        IO.puts "x: #{x} + acc: #{acc} = #{x <> acc}"
-        x <> acc
-    end)
+  IO.puts "x: #{x} + acc: #{acc} = #{x <> acc}"
+  x <> acc
+end)
 
 
 
