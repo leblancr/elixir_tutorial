508fffdeb71628cbb827ddf7928db9cc8f194c55
leblancr <rich.leblanc1@proton.me>
2024-10-02 22:27:48 -0400

    finished Control Structures

--- tut.exs
+++ tut.exs
@@ -117,6 +117,68 @@ res = Enum.map([1,2,3], &Adding.plus_three/1)
 IO.puts "map([1,2,3], &Adding.plus_three/1)
 result: #{inspect(res)}\n"
 
+#
+if "Hello" do
+  IO.puts "Valid string!"
+else
+  IO.puts "Invalid string."
+end
 
+# unless = like if not, like if is true
+unless is_integer("hello") do
+  IO.puts "Not an Int"
+end
 
-# The match operator performs assignment when the left side of the match includes a variable.
+# case, ok matches and string is assigned to variable
+res = case {:ok, "Hello World"} do
+  {:ok, result} -> result
+  {:error, _reason} -> "Uh oh!"
+  _ -> "Catch all" # _ as the else that will match “everything else”.
+end
+IO.puts "result: #{inspect(res)}\n"
+
+# error matches and string is assigned to variable
+result = case {:error, "Something went wrong"} do
+  {:ok, result} -> result
+  {:error, reason} -> reason <> "re" # This will match now
+  _ -> "Catch all"
+end
+IO.puts result
+
+# ^ pin operator means
+pie = 3.14
+3.14
+
+# pin (^) keeps original value, does not match or reassign
+result = case "cherry pie" do
+  ^pie -> "Not so tasty"
+  pie -> "I bet #{pie} is tasty"
+end
+IO.puts result
+
+# with guard
+result = case {1, 2, 3} do
+  {1, x, 3} when x > 5 ->
+    "Will match"
+  _ ->
+    "Won't match"
+end
+IO.puts result
+
+#
+result = cond do
+  2 + 2 == 5 ->
+    "This will not be true"
+  2 * 2 == 3 ->
+    "Nor this"
+  1 + 1 == 2 ->
+    "But this will"
+end
+IO.puts result
+
+# with
+user = %{first: "Sean", last: "Callan"}
+result = with {:ok, first} <- Map.fetch(user, :first),
+              {:ok, last} <- Map.fetch(user, :last),
+              do: last <> ", " <> first
+IO.puts result
